a8228a45cc594841fa38669412d93b66dc62c2f7
Asplap3275 <kuralanban.karthi@aspiresys.com>
2023-02-24 17:29:47 +0530

    root file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const http =require('http');
 const routes=require('./routes')
-console.log(routes.text);
+// console.log(routes.text);
+// console.log('kural');
+// console.log('wid');
 
 const server=http.createServer(routes.handler);
 server.listen(4000);
