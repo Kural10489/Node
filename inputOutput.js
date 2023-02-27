2e332019484b53617bfa714dc444de03a77ee9fe
Asplap3275 <kuralanban.karthi@aspiresys.com>
2023-02-27 17:50:00 +0530

    praticed Input output in node

---
+++ inputOutput.js
@@ -0,0 +1,20 @@
+// const input=require('readline').createInterface({
+//     input:process.stdin,
+//     output:process.stdout,
+// });
+// input.question(`what's your name`,name=>{
+//     console.log(`Hi${name}!!`);
+
+// });
+// const repl=require('repl');
+// repl.start();
+// const local=repl.start(`$ `);
+// local.on('exit',()=>{
+//     console.log("exiting the rppl");
+//     process.exit();
+// })
+
+// const name=process.stdin.on('data',(data)=>{
+//     console.log(data.toString());
+// })
+
