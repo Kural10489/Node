c5757506185155bad0964efdab95ca93f6332b67
Asplap3275 <kuralanban.karthi@aspiresys.com>
2023-02-27 17:50:31 +0530

    Created Local  Modules

--- localModules.js
+++ localModules.js
@@ -1,5 +1,13 @@
-const math=require('./test')
-const data=require('./data')
-console.log(math.add(2,3));
-console.log(math.sub(8,1));
-console.log(data);
+// const math=require('./test')
+// const data=require('./data')
+// console.log(math.add(2,3));
+// console.log(math.sub(8,1));
+// console.log(data);
+arr=[{name:'kural',age:22},{name:'Moni',age:22},{name:'tyson',age:6}]
+arr2=[1,1,1]
+console.log(
+
+    // arr.filter((a)=>{ return a.name=='kural'})
+    arr2.reduce((a,b)=>{ return a+b})
+);
+
