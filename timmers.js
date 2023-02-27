644f9d85dac286209bbbcdb5f87ba17bdc6c23fd
Asplap3275 <kuralanban.karthi@aspiresys.com>
2023-02-27 17:51:18 +0530

    Praticed Timmers in node

---
+++ timmers.js
@@ -0,0 +1,24 @@
+// const timeout=()=>{
+//     console.log('hello');
+// }
+// setTimeout(timeout,2000);
+// setTimeout(timeout,5000);
+
+// const timerId=setTimeout(()=>{},0);
+// console.log("l");
+// clearTimeout(timerId);
+
+// let count=0;
+
+// const print=()=>{
+//     console.log("Hii");
+//     count+=1;
+//     if(count==5){
+//         console.log("Done");
+//         clearInterval(timerId);
+//     }
+// }
+// const timerId=setInterval(print,1000);
+obj=[{id:1,name:'kueal',age:21},{id:1,name:'kueal',age:21}].o
+unique=new Set(obj);
+console.log(unique);
