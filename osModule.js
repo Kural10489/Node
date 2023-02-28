a927b96b06b17b7882ad9167aab0bef7dd403532
Asplap3275 <kuralanban.karthi@aspiresys.com>
2023-02-28 17:37:17 +0530

    Implemented Os Module

---
+++ osModule.js
@@ -0,0 +1,8 @@
+const os=require('os');
+console.log(os.cpus());
+console.log(os.endianness());
+console.log(os.freemem());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.version());
+console.log(os.platform());
