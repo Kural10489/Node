8b4b10abc3d57375f5f17d3eb3a830a9d0c47f0e
Asplap3275 <kuralanban.karthi@aspiresys.com>
2023-03-01 17:56:25 +0530

    Implemented Net module

---
+++ netModule/client.js
@@ -0,0 +1,11 @@
+var net =require('net');
+var client = net.connect({port:9080,function(){
+console.log("Connected to Server");
+}})
+client.on('data',(data)=>{
+    console.log(data.toString());
+    client.end();
+})
+client.on('end',()=>{
+    console.log("Disconned from the server");
+})
---
+++ netModule/server.js
@@ -0,0 +1,13 @@
+var net = require('net');
+var server=net.createServer((connection)=>{
+    console.log("Client Connected");
+    connection.on('end',()=>{
+        console.log(`client is disconnected`);
+    });
+    connection.write(`hello World!\r\n`);
+    connection.pipe(connection);
+    
+});
+server.listen(9080,()=>{
+    console.log(`Server is Listening..`);
+})
