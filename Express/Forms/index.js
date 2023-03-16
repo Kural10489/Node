edc434b3458c87f15b03e6acbfddda4441da3211
Asplap3275 <kuralanban.karthi@aspiresys.com>
2023-03-16 15:12:42 +0530

    Inital commit

---
+++ Express/Forms/index.js
@@ -0,0 +1,10 @@
+var express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = 
+        require("passport-local-mongoose")
+const User = require("./model/User");
+var app = express();
+  
